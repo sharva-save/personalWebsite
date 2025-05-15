eb37d0bd22904f6cef9275fa74b83f68466b50e4
sharva-save <sharvasave2509@gmail.com>
2025-05-15 15:20:19 +0530

    deployement changes

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,7 +1,7 @@
 import React from "react"; 
 import { words } from "../constants";
 import Button from "../components/Button";
-import HeroExperince from "../components/HeroModels/HeroeXPERINCE.JSX";
+import HeroExperince from "../components/HeroModels/HeroExperince";
 
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
